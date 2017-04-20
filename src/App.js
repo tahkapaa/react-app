d2ef500fc271c2e0e1d554583dbf93c84d8507aa
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-04-20 14:27:38 +0300

    First version of course react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from './Header'
+import Button from './Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Header />
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Button label='Nappi 1:'/>
+        <Button counter={10}/>
+        <Button counter={100}/>
+        <Button counter={1000}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import './Header.css'
+
+
+class Header extends Component {
+    
+    componentWillMount() {
+        console.log('componentWIllMount!');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount!');
+    }
+
+    render() {
+        console.log('render!');
+        const muuttuja = 'Jotain tekstiä!';
+
+        return (
+            <main>
+                <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Tervetuloa</h2>
+                {muuttuja}
+                </div>
+            </main>
+        );
+    }
+}
+
+export default Header;
