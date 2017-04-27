ace70391bb9bedef3498bd2b0771f9ff455497ba
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-04-27 12:37:19 +0300

    Use airbnb linter config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './Header'
-import Button from './Button'
+import Header from './Header';
+import Button from './Button';
 
 class App extends Component {
   render() {
