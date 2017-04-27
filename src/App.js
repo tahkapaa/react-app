8352127c19b5467a0508d1a7693a57510978a4b9
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-04-27 12:52:53 +0300

    Add vscode settings

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
         <Header />
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <Button label='Nappi 1:'/>
-        <Button counter={10}/>
-        <Button counter={100}/>
-        <Button counter={1000}/>
+        <Button label="Nappi 1:" />
+        <Button counter={10} />
+        <Button counter={100} />
+        <Button counter={1000} />
       </div>
     );
   }
