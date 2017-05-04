15a40f94de31d235bc29fdaf9e88c2d4744f38dd
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-05-04 14:07:39 +0300

    Add counter using redux

--- src/About.js
+++ src/About.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
 
 class About extends Component {
   render() {
     return (
       <div>
-        About
+        <h1>About</h1>
+        <div>
+          Counter value: {this.props.counter}
+        </div>
+        <div><button onClick={this.props.increment}>+</button></div>
+        <div><button onClick={this.props.decrement}>-</button></div>
       </div>
     );
   }
 }
 
-export default About;
+// function mapStateToProps(state) {
+//   return {
+//     counter: state.counter,
+//   };
+// }
+
+// const connectToStore = connect(mapStateToProps);
+// const ConnectedAbout = connectToStore(About);
+// export default ConnectedAbout;
+// ... = ->
+export default connect(
+  function mapStateToProps(state) {
+    return {
+      counter: state.counter,
+    };
+  },
+  function mapDispatchToProps(dispatch) {
+    return {
+      increment: () => dispatch({ type: 'INCREMENT' }),
+      decrement: () => dispatch({ type: 'DECREMENT' }),
+    };
+  },
+)(About);
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,40 @@
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import { createLogger } from 'redux-logger';
+
 import App from './App';
 import './index.css';
 
+
+function counterReducer(state = 10, action) {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  }
+  if (action.type === 'DECREMENT') {
+    return state - 1;
+  }
+  return state;
+}
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const middleWare = applyMiddleware(createLogger());
+
+const store = createStore(rootReducer, middleWare);
+
 const router = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(
